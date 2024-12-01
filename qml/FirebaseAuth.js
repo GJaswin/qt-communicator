1abe816c363fac4b71fb23fe0802fca0b0348555
GJaswin <gjaswink@gmail.com>
2024-12-01 14:53:18 +0530

    Merged signup and setusername functions

--- qml/FirebaseAuth.js
+++ qml/FirebaseAuth.js
@@ -3,31 +3,60 @@ Qt.include("apikey.js")
 var apiKey = apikey;
 
 //User data
-var idToken;
-var uid;
-var user_email;
 
-function signUp(email, password, callback) {
-    var xhr = new XMLHttpRequest();
+function signUp(username, email, password, callback) {
+    var xhrSignUp = new XMLHttpRequest();
     var url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
-    var data = {
+    var signUpData = {
         email: email,
         password: password,
         returnSecureToken: true,
     };
 
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            var response = JSON.parse(xhr.responseText);  // Parse response
-            callback(response);
-            idToken = response.idToken;
-            uid = response.localId;
-            user_email = response.email;
+    xhrSignUp.open("POST", url);
+    xhrSignUp.setRequestHeader("Content-Type", "application/json");
+    xhrSignUp.onreadystatechange = function () {
+        if (xhrSignUp.readyState === XMLHttpRequest.DONE) {
+            var response = JSON.parse(xhrSignUp.responseText);
+
+            if (xhrSignUp.status === 200) {
+                var idToken = response.idToken;
+                var uid = response.localId;
+
+                // Callback with signup response
+                callback(response);
+
+                // Make the Firestore request to save username
+                var xhrFirestore = new XMLHttpRequest();
+                const firestoreApiUrl = `https://firestore.googleapis.com/v1/projects/qt-communicator/databases/(default)/documents/users/${uid}`;
+                var firestoreData = {
+                    fields: {
+                        username: {
+                            stringValue: username,
+                        },
+                    },
+                };
+
+                xhrFirestore.open("PATCH", firestoreApiUrl, true);
+                xhrFirestore.setRequestHeader("Authorization", `Bearer ${idToken}`);
+                xhrFirestore.setRequestHeader("Content-Type", "application/json");
+                xhrFirestore.onreadystatechange = function () {
+                    if (xhrFirestore.readyState === XMLHttpRequest.DONE) {
+                        if (xhrFirestore.status === 200) {
+                            console.log("Username set successfully in Firestore");
+                        } else {
+                            console.error("Error setting username in Firestore:", xhrFirestore.responseText);
+                        }
+                    }
+                };
+                xhrFirestore.send(JSON.stringify(firestoreData));
+            } else {
+                console.error("Error during signup:", response.error.message);
+                callback(response);
+            }
         }
     };
-    xhr.send(JSON.stringify(data));
+    xhrSignUp.send(JSON.stringify(signUpData));
 }
 
 function login(email, password, callback) {
@@ -51,32 +80,5 @@ function login(email, password, callback) {
         }
     };
     xhr.send(JSON.stringify(data));
-}
 
-
-function setUsername(username) {
-    const firestoreApiUrl = `https://firestore.googleapis.com/v1/projects/qt-communicator/databases/(default)/documents/users/${uid}`;
-
-    const data = {
-        fields: {
-            username: {
-                stringValue: username,
-            },
-        },
-    };
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("PATCH", firestoreApiUrl, true);
-    xhr.setRequestHeader("Authorization", `Bearer ${idToken}`);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                console.log('Username set successfully in Firestore');
-            } else {
-                console.error('Error setting username in Firestore:', xhr.responseText);
-            }
-        }
-    };
-    xhr.send(JSON.stringify(data));
 }
